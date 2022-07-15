45bff7724c9a5080d2ffe7b37441b34cd1cfac4b
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-07-15 15:53:53 +0200

    created component to get Event details

---
+++ src/Event.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class Event extends Component {
+    state = {
+        collapsed: true,
+    };
+
+    handleClick = () => {
+        this.setState({ collapsed: !this.state.collapsed });
+    };
+
+    dateNewFormat = (eventDate) => {
+        const newDate = `${new Date(eventDate)}`;
+        return newDate;
+    };
+
+    changeBtnText = () => {
+        return `${this.state.collapsed ? 'show' : 'hide'} details`;
+    };
+
+    render() {
+        const { event } = this.props;
+
+        return (
+            <div className="event">
+                <h3 className="title">{event.summary}</h3>
+                <p className="start-time">{this.dateNewFormat(event.start.dateTime)}</p>
+                <p className="location">{event.location}</p>
+                {!this.state.collapsed && (
+                    <p className="event-details">{event.description}</p>
+                )}
+                <button className="btn-details" onClick={this.handleClick}>
+                    {this.changeBtnText()}
+                </button>
+            </div>
+        );
+    }
+}
+export default Event;
