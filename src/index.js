979a2f7ecaed7372889a05934ecfca4dd17b2bfe
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-07-22 15:50:37 +0200

    imported atatus monitoring tool

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('bc5554c12b53404b872122a1bc6e1b42').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
