4a01b56a5ca6f9187658c005235bab5cf7c7bf8a
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-07-22 13:31:48 +0200

    added comments on test

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import EventList from '../EventList';
-
-import { mockData } from '../mock-data';
 import Event from '../Event';
+import { mockData } from '../mock-data';
 
 describe('<EventList /> component', () => {
+
+    /* FEATURE 1: FILTER EVENTS BY CITY*/
+
+    /* SCENARIO 1: By default, when a user hasn’t searched for a city, show upcoming events 
+    from all locations.*/
+
     test('render correct number of events', () => {
         const EventListWrapper = shallow(<EventList events={mockData} />);
         expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
     });
-});
+
+
+}); 
